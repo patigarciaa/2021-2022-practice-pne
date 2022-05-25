179f6a31ef9ca9c86dda3f784e565c23e15b499c
patix <px.garcia.2021@alumnos.urjc.es>
2022-05-25 11:39:16 +0200

    advance terminado

--- final-project/client.py
+++ final-project/client.py
@@ -32,7 +32,7 @@ def request_server(endpoint, params=""):
         else:
             print("THERE WERE SOME CONNECTION PROBLEMS, TRY AGAIN LATER")
     except http.client.InvalidURL:
-        print("THE URL COULD NOT BE FORMED, CHECK THE ENDPOINT AND PARAMS. THEY CAN NOT HAVE SPACES.")
+        print({"error": "THE URL COULD NOT BE FORMED, CHECK THE ENDPOINT AND PARAMS. THEY CAN NOT HAVE SPACES."})
 
 #basic
 termcolor.cprint("THE LIST IS:", "green")
@@ -41,35 +41,35 @@ print(data_list_species)
 
 
 termcolor.cprint("THE KARYOTYPE IS:", "green")
-data_karyotype = request_server("/karyotype?", "species2= human&json=on")
+data_karyotype = request_server("/karyotype?", "species2= human&json=1")
 print(data_karyotype)
 
 
 termcolor.cprint("THE LENGTH IS:", "green")
-data_chromolength = request_server("/chromosomeLength?", "species3=human&chromosome=1&json=on")
+data_chromolength = request_server("/chromosomeLength?", "species3=human&chromosome=1&json=1")
 print(data_chromolength)
 
 
 
 #medium
 termcolor.cprint("THE SEQUENCE IS:", "green")
-data_seq = request_server("/geneSeq?", "gene_seq=FRAT1&json=on") #que pasa si mete un espacio
+data_seq = request_server("/geneSeq?", "gene_seq=FRAT1&json=1") #que pasa si mete un espacio
 print(data_seq)
 
 
 
 termcolor.cprint("THE INFO IS:", "green")
-data_info = request_server("/geneInfo?", "gene_info=FRAT1&json=on")
+data_info = request_server("/geneInfo?", "gene_info=FRAT1&json=1")
 print(data_info)
 
 
 termcolor.cprint("THE CALCULATIONS ARE:", "green")
-data_calc = request_server("/geneCalc?", "gene_calc=FRAT1&json=on")
+data_calc = request_server("/geneCalc?", "gene_calc=FRAT1&json=1")
 print(data_calc)
 
 
 termcolor.cprint("THE LIST IS:", "green")
-data_list = request_server("/geneList?", "gene_list=9&start_limit=22125500&end_limit=22136000&json=on")
+data_list = request_server("/geneList?", "gene_list=9&start_limit=22125500&end_limit=22136000&json=1")
 print(data_list)
 
 
